c3dfdf717e94344d4c3869420569235cffc1837e
Dean Bowler <dean.bowler@oakbrookfinance.com>
2020-08-23 14:16:07 +0100

    Fix sequelize pg config

--- lib/database/sequelizeConnection.ts
+++ lib/database/sequelizeConnection.ts
@@ -1,3 +1,4 @@
+import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
 
 console.info(process.env.DB_USER, process.env.DB_SCHEMA, process.env.DB_PASSWORD);
@@ -10,6 +11,7 @@ const sequelizeConnection = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     port: (process.env.DB_PORT ? Number.parseInt(process.env.DB_PORT) : undefined) ?? 5430,
     dialect: 'postgres',
+    dialectModule: pg,
     dialectOptions: {
       ssl: process.env.DB_SSL == 'true',
     },
